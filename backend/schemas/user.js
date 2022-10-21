c9558ceb1e5c9a64a8bb9bd5017d69ce342ef9d4
Raunak Kedia <raunakkedia1@gmail.com>
2022-10-21 20:54:47 +0530

    sanity schema setup

---
+++ backend/schemas/user.js
@@ -0,0 +1,22 @@
+export default {
+	name: "user",
+	type: "document",
+	title: "User",
+	fields: [
+		{
+			name: "userName",
+			type: "string",
+			title: "UserName",
+		},
+		{
+			name: "avatar",
+			type: "string",
+			title: "Avatar",
+		},
+		{
+			name: "email",
+			type: "string",
+			title: "Email",
+		},
+	],
+};
